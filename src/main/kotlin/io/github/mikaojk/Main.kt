d688cf78ff4bf6a8e669c8dd1ba8445cf28227e7
Joakim Taule Kartveit <joakim.taule.kartveit@nav.no>
2025-01-06 15:22:32 +0100

    first commit

--- src/main/kotlin/io/github/mikaojk/Main.kt
+++ src/main/kotlin/io/github/mikaojk/Main.kt
@@ -1,33 +1,72 @@
 package io.github.mikaojk
 
-import io.github.mikaojk.game.GameState
-import io.github.mikaojk.game.Player
-import io.github.mikaojk.game.Soldier
-
 
 fun main() {
-    println("Going to start game")
 
-    val initialGameState = GameState(
-        playerOne = Player(name= "Computer", soilders = (1..100).map { Soldier(it) }),
-        playerTwo = Player(name = "Joakim", soilders = (1..100).map { Soldier(it) })
-    )
+    val battlefields = 5
 
+    val playerOne = Player(name = "Computer", soilders = 100, wins = 0)
+    val playerTwo = Player(name = "Joakim", soilders = 100, wins = 0)
 
-    //val coumptersSoldiersToBattel = decideSoldiersToSendToBattel(initialGameState)
-    /*
-        coumptersSoldiersToBattel.forEach { soldierToBattel: Soldier ->
-        coumptersSoliders.filter { soldier: Soldier ->
-            soldierToBattel.number == soldier.number
+    for (i in 0 until battlefields) {
+        println("Round number: $i")
+
+        val playerOneSoildersSendToBattlefield = decideSoldiersToSendToBattel(playerOne.soilders)
+        val playerTwoSoildersSendToBattlefield = decideSoldiersToSendToBattel(playerTwo.soilders)
+
+        if (playerOneSoildersSendToBattlefield > playerTwoSoildersSendToBattlefield) {
+            println("${playerOne.name} winner")
+            playerOne.wins++
+        } else if (playerTwoSoildersSendToBattlefield > playerOneSoildersSendToBattlefield) {
+            println("${playerTwo.name} winner")
+            playerTwo.wins++
+        } else {
+            println("Draw")
         }
+        playerOne.soilders = (playerOne.soilders - playerOneSoildersSendToBattlefield)
+        playerTwo.soilders = (playerTwo.soilders - playerTwoSoildersSendToBattlefield)
+        println("\n")
+
     }
-    */
-
-    // coumptersSoldiersToBattel.filter {  }
-
-   // val battelResult = battle(coumptersSoldiersToBattel, yourSoliders)
-
-    //println(battelResult)
-
-
+    printGameResult(playerOne, playerTwo)
 }
+
+fun getWinner(playerOne: Player, playerTwo: Player): Player? {
+    val winner: Player? = when {
+        bothPlayerHaveSameNumberOfWins(playerOne, playerTwo) -> null
+        else -> listOf(playerOne, playerTwo)
+            .maxBy(Player::wins)
+    }
+    return winner
+}
+
+fun bothPlayerHaveSameNumberOfWins(playerOne: Player, playerTwo: Player): Boolean {
+    return (playerOne.wins == playerTwo.wins)
+}
+
+fun getLoser(playerOne: Player, playerTwo: Player): Player {
+    return listOf(playerOne, playerTwo)
+        .minBy(Player::wins)
+}
+
+fun printGameResult(playerOne: Player, playerTwo: Player) {
+    val winner = getWinner(playerOne, playerTwo)
+    println("Game is ended")
+    if (winner == null) {
+        println("The game ended in a draw")
+    } else {
+        val loser = getLoser(playerOne, playerTwo)
+        println("Winner is:  ${winner.name} with number of wins: ${winner.wins}")
+        println("Loser is:  ${loser.name} with number of wins: ${loser.wins}")
+    }
+}
+
+fun decideSoldiersToSendToBattel(numberOfAvibaleSoldiers: Int): Int {
+    return (0..numberOfAvibaleSoldiers).random()
+}
+
+data class Player(
+    val name: String,
+    var soilders: Int,
+    var wins: Int
+)
--- src/main/kotlin/io/github/mikaojk/game/GameState.kt
+++
@@ -1,6 +0,0 @@
-package io.github.mikaojk.game
-
-internal data class GameState(
-    val playerOne: Player,
-    val playerTwo: Player,
-)
